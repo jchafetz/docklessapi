79805ac601efc1cbc529528b1cbe95fbaed76c51
Aaron <achafetz@usaid.gov>
2018-09-14 08:51:15 -0400

    make all functions explicit

--- R/cleanFields.R
+++ R/cleanFields.R
@@ -14,19 +14,19 @@ cleanFields <- function(df){
 
     df <-
         df %>%
-        mutate(company = as.character(dfname))
+        dplyr::mutate(company = as.character(dfname))
 
     #Vehicle Type
     #If else statement to separate lime's bikes & scooters from the rest of companies
     if("attributes.vehicle_type" %in% colnames(df)){
         df <-
             df %>%
-            rename(vehicletype = attributes.vehicle_type)
+            dplyr::rename(vehicletype = attributes.vehicle_type)
     }
     else{
         df <-
             df %>%
-            mutate(
+            dplyr::mutate(
                 vehicletype = case_when(
                     company == "jump" ~ "bike",
                     company == "spin" ~ "bike",
@@ -39,37 +39,37 @@ cleanFields <- function(df){
     #BikeID
     name_ID <-
         df %>%
-        select(matches("bike_id|bikes.bike_id|attributes.plate_number")) %>%
+        dplyr::select(dplyr::matches("bike_id|bikes.bike_id|attributes.plate_number")) %>%
         names()
 
     df <-
         df %>%
-        rename(bikeID = name_ID)
+        dplyr::rename(bikeID = name_ID)
 
     #Longtitude & Latitude
     name_long <-
         df %>%
-        select(matches("lon|lng")) %>%
+        dplyr::select(dplyr::matches("lon|lng")) %>%
         names()
 
     name_lat <-
         df %>%
-        select(matches("lat|latitude")) %>%
+        dplyr::select(dplyr::matches("lat|latitude")) %>%
         names()
 
     df <-
         df %>%
-        rename(lat = name_lat,
-               long = name_long) %>%
-        mutate(lat = as.numeric(lat),
-               long = as.numeric(long)) %>%
-        filter(lat > 37 & lat < 39,
-               long > -78 & long < -76) #Skip reports data nationally
+        dplyr::rename(lat = name_lat,
+                      long = name_long) %>%
+        dplyr::mutate(lat = as.numeric(lat),
+                      long = as.numeric(long)) %>%
+        dplyr::filter(lat > 37 & lat < 39,
+                      long > -78 & long < -76) #Skip reports data nationally
 
     #Selecting fields we want
     df <-
         df %>%
-        select(company, vehicletype, bikeID, lat, long)
+        dplyr::select(company, vehicletype, bikeID, lat, long)
 
     return(df)
 
