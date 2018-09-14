a8c13b5a052e20cba08ea4bce4603bcb18850a7f
Aaron <achafetz@usaid.gov>
2018-09-14 10:00:35 -0400

    convert to a tibble for use with map

--- R/getDocklessdata.R
+++ R/getDocklessdata.R
@@ -2,11 +2,15 @@
 getDocklessdata <- function(){
 
     #Set the urls
-    skipurl <- "https://us-central1-waybots-production.cloudfunctions.net/dcFreeBikeStatus" #bikes
-    spinurl <- "https://web.spin.pm/api/gbfs/v1/free_bike_status" #data
-    birdurl <- "https://dzn9sy8hnh1q4.cloudfront.net/" #data
-    jumpurl <- "https://dc.jumpmobility.com/opendata/free_bike_status.json" #data
-    limeurl <- "https://lime.bike/api/partners/v1/bikes?region=Washington%20DC%20Proper"
+    dockless_urls <-
+        tibble::tribble(
+            ~company,                                                                         ~url,  ~layer,                            ~auth,
+            "skip", "https://us-central1-waybots-production.cloudfunctions.net/dcFreeBikeStatus", "bikes",                               NA,
+            "spin",                           "https://web.spin.pm/api/gbfs/v1/free_bike_status",  "data",                               NA,
+            "bird",                                      "https://dzn9sy8hnh1q4.cloudfront.net/",  "data",                               NA,
+            "jump",                 "https://dc.jumpmobility.com/opendata/free_bike_status.json",  "data",                               NA,
+            "lime",    "https://lime.bike/api/partners/v1/bikes?region=Washington%20DC%20Proper",  "data", "Bearer limebike-PMc3qGEtAAXqJa"
+        )
 
     #Pull the data
     skip <- bikeData(skipurl, "bikes", NA)
