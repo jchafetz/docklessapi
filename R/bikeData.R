d2d287e0596305b161c5d4178282b398685170b1
Aaron <achafetz@usaid.gov>
2018-09-14 08:32:55 -0400

    separate bikeData out as own function

---
+++ R/bikeData.R
@@ -0,0 +1,16 @@
+#Function to pull data from the API
+bikeData <- function(url, layer, auth){
+
+    #Pull the JSON
+    json <- url %>%
+        httr::GET(httr::timeout(60), add_headers(Authorization = paste(auth))) %>%
+        httr::content("text") %>%
+        jsonlite::fromJSON(flatten = TRUE)
+
+    #extract key elements
+    df <- purrr::pluck(json, layer) %>%
+        as.data.frame()
+
+    #Return dataframe
+    return(df)
+}
