ffd6dcefad8326dc1c61c585d551d234bbcffdd8
Aaron <achafetz@usaid.gov>
2018-09-14 17:32:46 -0400

    move comment before if statement

--- R/getDocklessdata.R
+++ R/getDocklessdata.R
@@ -14,11 +14,11 @@ getDocklessdata <- function(output_filepath = NULL){
     dockless_urls <-
         tibble::tribble(
             ~company,                                                                         ~url,  ~layer,                            ~auth,
-            "skip", "https://us-central1-waybots-production.cloudfunctions.net/dcFreeBikeStatus", "bikes",                               NA,
-            "spin",                           "https://web.spin.pm/api/gbfs/v1/free_bike_status",  "data",                               NA,
-            "bird",                                      "https://dzn9sy8hnh1q4.cloudfront.net/",  "data",                               NA,
-            "jump",                 "https://dc.jumpmobility.com/opendata/free_bike_status.json",  "data",                               NA,
-            "lime",    "https://lime.bike/api/partners/v1/bikes?region=Washington%20DC%20Proper",  "data", "Bearer limebike-PMc3qGEtAAXqJa"
+              "skip", "https://us-central1-waybots-production.cloudfunctions.net/dcFreeBikeStatus", "bikes",                               NA,
+              "spin",                           "https://web.spin.pm/api/gbfs/v1/free_bike_status",  "data",                               NA,
+              "bird",                                      "https://dzn9sy8hnh1q4.cloudfront.net/",  "data",                               NA,
+              "jump",                 "https://dc.jumpmobility.com/opendata/free_bike_status.json",  "data",                               NA,
+              "lime",    "https://lime.bike/api/partners/v1/bikes?region=Washington%20DC%20Proper",  "data", "Bearer limebike-PMc3qGEtAAXqJa"
         )
 
     #Pull the data
@@ -29,10 +29,10 @@ getDocklessdata <- function(output_filepath = NULL){
     #Timestamp new data and add it to data frame
     df$time <- lubridate::now()
 
+    #Time stamp and export or return if no folder path is given
     if(!is.null(output_filepath)){
-        #Time stamp and export
         time <- format(lubridate::now(), "%Y%m%d_%H%M%S_")
-        readr::write_csv(df, file.path(output_filepath, paste0(time, "docklessbike_data_set.csv")), na = "FALSE")
+        readr::write_csv(df, file.path(output_filepath, paste0(time, "docklessbike_data_set.csv")), na = "")
     } else {
         return(df)
     }
